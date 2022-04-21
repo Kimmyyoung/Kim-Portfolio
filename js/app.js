dc6fe85e1113a8926570d9b1259af2ff69e5f11f
kimmy <hiyoung.kimmy@gmail.com>
2022-04-21 16:23:47 -0700

    second commit (add mouse cursor function)

--- js/app.js
+++ js/app.js
@@ -1,74 +1,65 @@
+  $(document).ready(function(){
+    $("body").smoothWheel()
+  });
 
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
-import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
-import { SplitText } from "gsap-trial/SplitText";
+  let mouseCursor = document.querySelector(".cursor");
+  let navLinks = document.querySelectorAll(".nav .nav-link");
 
-gsapTrial.registerPlugin(SplitText, ScrollSmoother, ScrollTrigger);
-
-const smoother = ScrollSmoother.create({
-  smooth: 1.65,
-  effects: true
-});
-
-/* Hero */
-
-console.log("JavaScript Connect!");
-
-const splitTitle = new SplitText(".hero__title", { type: "words,chars" });
-const chars = splitTitle.chars;
-
-const tlHero = () =>
-  gsap
-    .timeline()
-    .to(chars, {
-      opacity: 0,
-      y: -100,
-      stagger: { from: "random", each: 0.01 }
-    })
-    .from(
-      ".sun__circle",
-      {
-        yPercent: 10,
-        scale: 0.5,
-        transformOrigin: "bottom center"
-      },
-      "<"
-    );
-
-ScrollTrigger.create({
-  trigger: ".hero",
-  start: "top top",
-  end: "bottom top",
-  pin: true,
-  scrub: 1,
-  animation: tlHero()
-});
-
-/* Lyrics */
-
-gsap.to(".asterisk__inner", {
-  rotate: 360 * 3,
-  scrollTrigger: {
-    trigger: ".hero",
-    endTrigger: ".lyrics",
-    start: "top top",
-    end: "bottom top",
-    scrub: 1
+  window.addEventListener("scroll", cursor);
+  window.addEventListener("mousemove", cursor);
+  
+  function cursor(e) {
+    mouseCursor.style.left = e.pageX + "px";
+    mouseCursor.style.top = e.pageY - scrollY + "px";
   }
+
+  navLinks.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      mouseCursor.classList.add("link-grow");
+      mouseCursor.style.zIndex = "-1";
+      link.classList.add("hovered-link");
+    });
+    link.addEventListener("mouseleave", () => {
+      mouseCursor.classList.remove("link-grow");
+      mouseCursor.style.zIndex = "1000";
+      link.classList.remove("hovered-link");
+    });
 });
 
-/* Marquee */
 
-let tlMarquee = () =>
-  gsap
-    .timeline()
-    .from(".marquee__a", { xPercent: 50 })
-    .from(".marquee__b", { xPercent: -50 }, "<");
 
-ScrollTrigger.create({
-  trigger: ".marquee",
-  start: "top bottom",
-  end: "bottom top",
-  scrub: 1,
-  animation: tlMarquee()
+
+
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
+  });
+  thhis.prepend('<div class="hero_cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".hero__title");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+}
+
+$(document).ready(function(){
+  // Now to start autoTyping just call the autoType function with the 
+  // class of outer div
+  // The second paramter is the speed between each letter is typed.   
+  autoType(".type-js",200);
 });
