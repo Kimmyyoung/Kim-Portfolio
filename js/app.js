b9177bdb9b8c53298e30e6b8d565d6b6cc6f49ec
kimmy <hiyoung.kimmy@gmail.com>
2022-04-25 11:52:10 -0700

    nav bar function & auto typing function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-  $(document).ready(function(){
-    $("body").smoothWheel()
-  });
+import { gsap } from "../gsap-public/src/all.js";
+import { ScrollTrigger } from "../gsap-public/src/ScrollTrigger.js";
 
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+
+//MouseCursor
   let mouseCursor = document.querySelector(".cursor");
   let navLinks = document.querySelectorAll(".nav .nav-link");
 
@@ -26,40 +31,117 @@
     });
 });
 
+//Main Text Rotate
+const TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-function autoType(elementClass, typingSpeed){
-  var thhis = $(elementClass);
-  thhis.css({
-    "position": "relative",
-    "display": "inline-block"
-  });
-  thhis.prepend('<div class="hero_cursor" style="right: initial; left:0;"></div>');
-  thhis = thhis.find(".hero__title");
-  var text = thhis.text().trim().split('');
-  var amntOfChars = text.length;
-  var newString = "";
-  thhis.text("|");
-  setTimeout(function(){
-    thhis.css("opacity",1);
-    thhis.prev().removeAttr("style");
-    thhis.text("");
-    for(var i = 0; i < amntOfChars; i++){
-      (function(i,char){
-        setTimeout(function() {        
-          newString += char;
-          thhis.text(newString);
-        },i*typingSpeed);
-      })(i+1,text[i]);
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
-  },1500);
-}
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #f47731 }";
+  document.body.appendChild(css);
+};
 
-$(document).ready(function(){
-  // Now to start autoTyping just call the autoType function with the 
-  // class of outer div
-  // The second paramter is the speed between each letter is typed.   
-  autoType(".type-js",200);
+
+
+const tlHero = () =>
+  gsap
+    .timeline()
+    .to("top", {
+      opacity: 0,
+      y: -100,
+      stagger: { from: "random", each: 0.01 }
+    })
+    .from(
+      ".sun__circle",
+      {
+        yPercent: 10,
+        scale: 0.5,
+        opacity: 40,
+        transformOrigin: "bottom center"
+      },
+      "<"
+    );
+
+const tl = gsap.timeline({
+  defaults: { ease: "none", transformOrigin: "50% 50%" },
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  }
 });
+
+const nav = document.querySelector("nav");
+
+
+tl.to(nav, { y: "-100%", duration: 0.16 }, 0)
+
+ScrollTrigger.create({
+  trigger: ".hero",
+  start: "top top",
+  end: "bottom top",
+  pin: true,
+  scrub: 1,
+  animation: tlHero()
+});
+
+gsap.to(".asterisk__inner", {
+  rotate: 360 * 3,
+  scrollTrigger: {
+    trigger: ".hero",
+    endTrigger: ".lyrics",
+    start: "top top",
+    end: "bottom top",
+    scrub: 1
+  }
+});
+
