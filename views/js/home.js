38722bfd1cb52b339e0c1636fb7010b41e097df8
kimmy <hiyoung.kimmy@gmail.com>
2022-06-07 16:23:55 -0700

    responvive page code update

--- views/js/home.js
+++ views/js/home.js
@@ -107,14 +107,14 @@ const nav = document.querySelector('.nav-items');
 
 
 menu.addEventListener('click', () => {
-  // nav.style.transform = "translateX(0%)";
-  nav.classList.toggle('nav-active');
-  
+  //nav.style.transform = "translateX(0%)";
+  nav.classList.toggle('.nav-active');
 });
 
 // Transforming to close btn
 $(".hamburger").click(function(){
   $(".hamburger").toggleClass("close");
+  $(".nav-items").toggleClass("nav-active");
 });
 
 
--- views/js/work.js
+++ views/js/work.js
@@ -22,4 +22,20 @@ $(document).ready(function(){
     $(this).addClass('work-active').siblings().removeClass('work-active');
   })
   
-  
+    
+// Mobile Menu 
+const menu = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-items');
+
+
+menu.addEventListener('click', () => {
+  //nav.style.transform = "translateX(0%)";
+  nav.classList.toggle('.nav-active');
+});
+
+// Transforming to close btn
+$(".hamburger").click(function(){
+  $(".hamburger").toggleClass("close");
+  $(".nav-items").toggleClass("nav-active");
+});
+
