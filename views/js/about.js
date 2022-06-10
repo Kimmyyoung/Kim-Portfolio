c4d0c9925e813d00285dcfe42d7727ad82366d36
kimmy <hiyoung.kimmy@gmail.com>
2022-06-10 15:15:19 -0700

    responsive function code

---
+++ views/js/about.js
@@ -0,0 +1,16 @@
+// Mobile Menu 
+const menu = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-items');
+
+
+menu.addEventListener('click', () => {
+  //nav.style.transform = "translateX(0%)";
+  nav.classList.toggle('.nav-active');
+});
+
+// Transforming to close btn
+$(".hamburger").click(function(){
+  $(".hamburger").toggleClass("close");
+  $(".nav-items").toggleClass("nav-active");
+});
+
