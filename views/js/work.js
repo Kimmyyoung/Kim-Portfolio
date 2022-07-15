300f27b3f64f6316226efdf6d50c791d7fd74cbe
kimmy <hiyoung.kimmy@gmail.com>
2022-07-15 15:39:11 -0700

    Feat: A new Features

--- views/js/work.js
+++ views/js/work.js
@@ -1,28 +1,4 @@
 
-// work page work-grid
-$(document).ready(function(){
-    $('.work-btn').click(function(){
-      const value= $(this).attr('data-filter');
-      if (value == 'all'){
-        $('.box').show('1000');
-      }else if(value == 'development'){
-        $('.development').show('1000');
-      }else if(value == 'design'){
-        $('.design').show('1000');
-      }
-     
-      $('.box').not('.'+value).hide('1000');
-      $('.box').work('.'+value).show('1000');
-  
-    })
-  })
-  
-  // add active class on selected tab
-  $('.work-btn').click(function(){
-    $(this).addClass('work-active').siblings().removeClass('work-active');
-  })
-
-
     
 // Mobile Menu 
 const menu = document.querySelector('.hamburger');
