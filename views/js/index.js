57a2dfeeccce308340ad3fd8c30e45d00ddedfc7
kimmy <hiyoung.kimmy@gmail.com>
2022-09-23 13:56:41 -0700

    ♻️ refactor: Update scss code for fixing bug 09-23

--- views/js/index.js
+++ views/js/index.js
@@ -59,21 +59,21 @@ const textrotate = function(el, toRotate, period) {
   
 
   
-// Mobile Menu 
-const menu = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-items');
+// // Mobile Menu 
+// const menu = document.querySelector('.hamburger');
+// const nav = document.querySelector('.nav-items');
 
 
-menu.addEventListener('click', () => {
-  //nav.style.transform = "translateX(0%)";
-  nav.classList.toggle('.nav-active');
-});
+// menu.addEventListener('click', () => {
+//   //nav.style.transform = "translateX(0%)";
+//   nav.classList.toggle('.nav-active');
+// });
 
-// Transforming to close btn
-$(".hamburger").click(function(){
-  $(".hamburger").toggleClass("close");
-  $(".nav-items").toggleClass("nav-active");
-});
+// // Transforming to close btn
+// $(".hamburger").click(function(){
+//   $(".hamburger").toggleClass("close");
+//   $(".nav-items").toggleClass("nav-active");
+// });
 
   
 /*----------------- Loading Page ------------------- */  
