a9284d75d2dc3724e15ed7274789d505c14159f1
kimmy <hiyoung.kimmy@gmail.com>
2022-09-30 16:36:06 -0700

    💎 style : Add Code for  Responsive Web

--- views/js/about.js
+++ views/js/about.js
@@ -1,15 +1,15 @@
-// Mobile Menu 
-const menu = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-items');
+// // Mobile Menu 
+// const menu = document.querySelector('.hamburger');
+// const nav = document.querySelector('.nav-items');
 
 
-menu.addEventListener('click', () => {
-  //nav.style.transform = "translateX(0%)";
-  nav.classList.toggle('.nav-active');
-});
+// menu.addEventListener('click', () => {
+//   //nav.style.transform = "translateX(0%)";
+//   nav.classList.toggle('.nav-active');
+// });
 
-// Transforming to close btn
-$(".hamburger").click(function(){
-  $(".hamburger").toggleClass("close");
-  $(".nav-items").toggleClass("nav-active");
-});
+// // Transforming to close btn
+// $(".hamburger").click(function(){
+//   $(".hamburger").toggleClass("close");
+//   $(".nav-items").toggleClass("nav-active");
+// });
